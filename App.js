9ec7712df37d41b5cff1231276c6dfd417c9280f
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2023-11-20 21:13:58 +0100

    Move app.js to src folder and add Tab Screens

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import UserProfile from "./src/components/pages/UserProfile";
+import FavoriteRecipes from "./src/components/pages/FavoriteRecipes";
+import RecipeGeneration from "./src/components/pages/RecipeGeneration";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello I'm GastroMind! How can I help you today?</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Favorite Recipes") {
+              iconName = focused ? "heart" : "heart";
+            } else if (route.name === "Recipe Generation") {
+              iconName = focused ? "add-circle-outline" : "add-circle-outline";
+            } else if (route.name === "User Profile") {
+              iconName = focused ? "person" : "person";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#C600AD",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Favorite Recipes" component={FavoriteRecipes} />
+        <Tab.Screen name="Recipe Generation" component={RecipeGeneration} />
+        <Tab.Screen name="User Profile" component={UserProfile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
